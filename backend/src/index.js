bd945cf3890f68a64050623b4c365ae9d6011eeb
Deepak Yadav <deepak160895@gmail.com>
2025-04-21 17:58:49 +0530

    fix(config): add missing doenv.config() for environment variables

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,18 @@
 import express from "express"
 import dotenv from "dotenv"
 
+
+dotenv.config()
+
+
 const app = express()
 const port = process.env.PORT || 8080
+
+app.use(express.json())
+
+app.get("/",(req,res)=>{
+    res.send("Welcome to LEETLAB🔥")
+})
 
 
 
