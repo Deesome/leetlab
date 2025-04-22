7025644a429d3c82e98b84ddcf30893420778bda
Deepak Yadav <deepak160895@gmail.com>
2025-04-22 17:51:34 +0530

    feat(utils): implement Cloudinary config and upload function

---
+++ backend/src/utils/cloudinary.js
@@ -0,0 +1,33 @@
+import { v2 as cloudinary } from 'cloudinary';
+import dotenv from "dotenv"
+
+
+dotenv.config()
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDIANRY_API_SECRET
+})
+
+async function uploadOnCloudinary(path) {
+    try {
+        if (!path) return null
+        const uploadResult = await cloudinary.uploader.upload(path, {
+             resource_type: "auto" })
+        //TODO - If file uplaod successfully on cloudinary then delete the file from server
+        return uploadResult
+    } catch (error) {
+        throw new ApiError(400,"Error : Error in uplaoding the file on CLoudinary")
+        //TODO - If file uplaod successfully on cloudinary then delete the file from server
+
+    }
+}
+
+
+export default uploadOnCloudinary
+
+
+
+
+
