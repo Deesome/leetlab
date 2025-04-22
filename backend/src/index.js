c99fdff2accf0f9f48aee796fe1902d43ae4987b
Deepak Yadav <deepak160895@gmail.com>
2025-04-23 00:06:33 +0530

    chore(cookie-parser):added cookieParser in index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express"
 import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
 
 
 
@@ -10,6 +11,8 @@ const app = express()
 const port = process.env.PORT || 8080
 
 app.use(express.json())
+app.use(cookieParser())
+
 
 app.get("/",(req,res)=>{
     res.send("Welcome to LEETLAB🔥")
