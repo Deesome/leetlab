f5ab2d62fe89807f25a82e15dacbd72a3648f6b9
Deepak Yadav <deepak160895@gmail.com>
2025-05-05 15:48:23 +0530

    Refactor(Execute controller): Refator submit contoller in execute.contoller.js

--- backend/src/controllers/execute.controllers.js
+++ backend/src/controllers/execute.controllers.js
@@ -1,6 +1,6 @@
 import { pollBatchResults, submitBatch } from "../libs/judge0.libs.js";
 import { db } from "../libs/db.js";
-import { json } from "express";
+
 
 const runCode = async (req, res) => {
   try {
@@ -75,10 +75,10 @@ const runCode = async (req, res) => {
 
 const submitCode = async (req, res) => {
   try {
-    const { source_code, language_id, stdin, expected_output, problemId } =
+    const { source_code, language_id, stdin, expected_outputs, problemId } =
       req.body;
     const userId = req.user.id;
-    console.log(source_code, language_id, stdin, expected_output, problemId);
+    console.log(source_code, language_id, stdin, expected_outputs, problemId);
     console.log(userId);
 
     if (!userId || !source_code || !problemId) {
@@ -90,8 +90,8 @@ const submitCode = async (req, res) => {
     if (
       !Array.isArray(stdin) ||
       stdin.length === 0 ||
-      !Array.isArray(expected_output) ||
-      expected_output.length !== stdin.length
+      !Array.isArray(expected_outputs) ||
+      expected_outputs.length !== stdin.length
     ) {
       return res.status(400).json({ error: "Invalid or Missing test cases" });
     }
@@ -144,60 +144,93 @@ const submitCode = async (req, res) => {
       });
     }
 
-   
+    let allPassed = true
 
-    // submission should be single
-    // store the data in sbmission model
-    // but in submission model stdin and stdout are not single , they are more then one , in db it is tring
+    const detailedResults = results.map((result,i)=>{
+      const stdout = result.stdout?.trim()
+      const expected_output = expected_outputs[i]?.trim()
+      const passed = stdout === expected_output
 
-    const testCaseResults = await Promise.all(results.map(async(result, i) => {
-      const passed = result.stdout.trim() === expected_output[i].trim();
+      if(!passed) allPassed = false
 
-      // returning object doesnot make sense
-      // create entries in DB here 
-      
-      return db.testCaseResult.create({
-        data:{
-          submissionId : submissionDB.id,
-          testCase: i + 1,
-          passed,
-          stdout: result.stdout,
-          expected: expected_output[i],
-          stderr: result.stderr,
-          compileOutput: result.compileOutput,
-          status: result.status.description,
-          memory: result.memory.toString(),
-          time: result.time,
+      return {
+        testCase : i+1,
+        passed,
+        stdout,
+        expected : expected_output,
+        stderr : result.stderr || null,
+        compile_output : result.compile_output || null,
+        status : result.status.description,
+        memory: result.memory ? `${result.memory} KB` : undefined,
+        time: result.time ? `${result.time} s` : undefined,
+      }
+    })
 
-        }
-        })
-     
+    console.log("detailed Result",detailedResults) // array of an result [{result},{result},{result}]
+
+    const testCaseResults = detailedResults.map((result)=>({
+      submissionId : submissionDB.id,
+      testCase : result.testCase,
+      passed : result.passed,
+      stdout : result.stdout,
+      expected : result.expected,
+      stderr : result.stderr,
+      compileOutput : result.compile_output,
+      status : result.status,
+      memory:result.memory,
+      time : result.time
+
     }))
 
-  
-    if(!testCaseResults){
-      return res.status(500).json({
-        error : "Internal Error while save testcase result to DB"
+    console.log("testCaseResult",testCaseResults) // array of an object
+
+    // createMany expects array of an object
+    await db.testCaseResult.createMany({
+      data : testCaseResults
+    })
+
+
+    if(allPassed){
+      await db.problemSolved.upsert({
+        where : {
+          userId_problemId : {
+            userId,
+            problemId
+          }
+        },
+        update : {},
+        create : {
+          userId,
+          problemId
+        }
       })
     }
 
-    console.log("testCaseResult",testCaseResults)
+    const submissionWithTestResult = await db.submission.findUnique({
+      where : {
+        id : submissionDB.id
+      },
+      include : {
+        testCases : true
+      }
+    })
 
-    
+    if(!submissionWithTestResult){
+      return res.status(500).json({
+        error : "Internal server Error while fetching submission with test Results from DB"
+      })
+    }
+
 
     res.status(200).json({
+      success :true,
       message: "Code executed",
-      submissionDB,testCaseResults
+      submissionWithTestResult
     });
 
-    if (!results || results.length === 0) {
-      return res
-        .status(500)
-        .json({ error: "Failed to get results from Judge0" });
-    }
   } catch (error) {
-    console.error(error);
-    return res.status(500).json({ error: "Internal Server Error" });
+    console.error("Error executing Code",error,"Error Message:",error.message);
+    return res.status(500).json({ error: "Failed to execute code" });
   }
 };
 
