207d83c5b07f216785bbd2078e1e7458132ca129
Deepak Yadav <deepak160895@gmail.com>
2025-05-05 17:10:23 +0530

    feat(Submission Controller): added getSubmissionForProblem and added getSubmissionCountForProblem and setup route for it

--- backend/src/controllers/submission.controllers.js
+++ backend/src/controllers/submission.controllers.js
@@ -34,6 +34,88 @@ const getAllSubmissions = async(req,res)=>{
 
 }
 
+const getSubmissionForProblem = async(req,res)=>{
+    // user must be login
+    // get problemid from params
+    //search the submission on the basis of problemid
+    // send back the response 
+
+    const userId = req.user.id
+    const problemId = req.params.problemId
+
+    if(!userId){
+        return res.status(403).json({
+            error : "User not allowed, You must login first"
+        })
+    }
+
+    if(!problemId){
+        return res.status(400).json({
+            error : " ProblemId is required"
+        })
+    }
+
+   try {
+     const submissionsForProblem = await db.submission.findMany({
+         where : {
+             userId,
+             problemId
+         }
+     })
+
+      res.status(200).json({
+            success:true,
+            message:"Submissions for problem fetched successfully",
+            submissionsForProblem
+      })
+   } catch (error) {
+        console.error("Fetch Submissions Error:", error);
+        res.status(500).json({ error: "Failed to fetch submissions for problem" });
+    
+   }
 
 
-export {getAllSubmissions}
+
+
+}
+
+const getSubmissionCountForProblem = async(req,res) =>{
+    const userId = req.user.id
+    const problemId = req.params.problemId
+
+    if(!userId){
+        return res.status(403).json({
+            error : "User not allowed, You must login first"
+        })
+    }
+
+    if(!problemId){
+        return res.status(400).json({
+            error : " ProblemId is required"
+        })
+    }
+
+    try {
+        const submissionCount = await db.submission.count({
+            where : {
+                userId,
+                problemId
+            }
+        })
+
+        res.status(200).json({
+            success:true,
+            message:"SubmissionCount Fetched successfully",
+            count:submissionCount
+        })
+        
+    } catch (error) {
+        console.error("Fetch SubmissionCount Error:", error);
+        res.status(500).json({ error: "Failed to fetch submissionCount" });
+        
+    }
+}
+
+
+
+export {getAllSubmissions,getSubmissionForProblem,getSubmissionCountForProblem}
--- backend/src/routes/submission.routes.js
+++ backend/src/routes/submission.routes.js
@@ -1,12 +1,13 @@
 import express , {Router} from "express"
 import { authMiddleware } from "../middlewares/auth.middleware.js"
-import { getAllSubmissions } from "../controllers/submission.controllers.js"
+import { getAllSubmissions, getSubmissionCountForProblem, getSubmissionForProblem } from "../controllers/submission.controllers.js"
 
 const submissionsRouter = Router()
 
 submissionsRouter.route("/get-all-submissions").get(authMiddleware,getAllSubmissions)
+submissionsRouter.route("/get-submissions-for-problem/:problemId").get(authMiddleware,getSubmissionForProblem)
 
-
+submissionsRouter.route("/submission-count/:problemId").get(authMiddleware,getSubmissionCountForProblem)
 
 
 export default submissionsRouter
